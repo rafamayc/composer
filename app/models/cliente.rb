e9c2aeee5092872155aa5357eb26358849111fd4
Rafamayc <rafael@genesistemas.net>
2014-06-06 00:00:19 -0300

    Tela Clientes2

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,10 +1,18 @@
 class Cliente < ActiveRecord::Base
-def self.search(search)
+   validates_length_of :telefone, :is => 10, :message => " - Deve ter dez digitos" 
+  def self.search(search)
     if search
      
-      find(:all, :conditions => ['name LIKE ?', "%#{search.capitalize}%"])
+      find(:all,:conditions => ['name LIKE ?', "%#{search.capitalize}%"],:order => "name")
     else
-      find(:all)
+      find(:all,:order => "name")
     end
   end
+  before_save :normalizar
+  
+  private
+  def normalizar
+    self.name.capitalize!
+  end
+  
 end
